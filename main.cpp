bb7a17641697c3ddda64f301286020545616fdc1
MahmoodAlHassan <mahmood.al-hassan@elev.ga.ntig.se>
2025-10-06 10:16:04 +0200

    -d-l-

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 // Frågar användaren om hens namn, och sedan skriver ut “Hej <användarens namn>.”
-    string namej;
+    string nam;
     cout << "Vad heter du? " << endl;
-    getline(cin, namej);
-    cout << "Hej " << namej << "." << endl;
+    getline(cin, nam);
+    cout << "Hej " << nam << "." << endl;
     return 0;
 }
