36d68c6f39d55a1185f15efbe2ee4d1171ba3f77
MahmoodAlHassan <mahmood.al-hassan@elev.ga.ntig.se>
2025-10-06 09:52:46 +0200

    Namn variabel

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+// Frågar användaren om hens namn, och sedan skriver ut “Hej <användarens namn>.”
+    string namn;
+    cout << "Vad heter du? " << endl;
+    getline(cin, namn);
+    cout << "Hej " << namn << "." << endl;
     return 0;
+
 }
